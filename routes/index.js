c7fec7318375230c9aaa98888c0b2c1829c3e1d9
peanutworld <peanutwrldcs@gmail.com>
2023-10-20 20:21:01 +0200

    # Added /pretraga route

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,19 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.use('/artikal', require('./artikal'));
-router.use('/artikli_lista', require('./artikli_lista'));
-router.use('/update', require('./update'));
-router.use('/ping', require('./ping'));
-router.use('/barcode', require('./barcode'));
+router.use("/artikal", require("./artikal"));
+router.use("/artikli_lista", require("./artikli_lista"));
+router.use("/update", require("./update"));
+router.use("/ping", require("./ping"));
+router.use("/barcode", require("./barcode"));
+router.use("/pretraga", require("./pretraga"));
 
-router.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+router.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-router.all("*", function(req, res) {
-	return res.status(404).send("Page not found");
+router.all("*", function (req, res) {
+  return res.status(404).send("Page not found");
 });
 
 module.exports = router;
---
+++ routes/pretraga.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const { Op } = require("sequelize");
+const ArtikliInventura = require("../database/models/artikli_inventura");
+
+const router = express.Router();
+
+router.get("/:searchTerm", async (req, res) => {
+  try {
+    const searchTerm = req.params.searchTerm;
+    const record = await ArtikliInventura.findOne({
+      where: {
+        [Op.or]: [
+          { art_ean: searchTerm },
+          { art_ean1: searchTerm },
+          { pdok_art: searchTerm },
+        ],
+      },
+    });
+
+    if (record) {
+      const data = {
+        pdok_art: record.pdok_art,
+        pdok_nart: record.pdok_nart,
+        art_ean: record.art_ean,
+        art_ean1: record.art_ean1,
+        pdok_jmj: record.pdok_jmj,
+        pdok_csp: record.pdok_csp.toFixed(2).toString(),
+        pdok_knj: record.pdok_knj.toFixed(2).toString(),
+        pdok_dat: new Date(record.pdok_dat).toUTCString(),
+        pdok_kol: record.pdok_kol.toFixed(2).toString(),
+        pdok_vrijeme_ocitanja: new Date(
+          record.pdok_vrijeme_ocitanja
+        ).toUTCString(),
+        art_ocitano: record.art_ocitano,
+        inv_user: record.inv_user,
+      };
+
+      res.json(data);
+    } else {
+      res.status(404).json({ error: "Item not found" });
+    }
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+module.exports = router;
