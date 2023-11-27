881e96eedb8a31785b45ec6bee6bd6168fe77139
peanutworld <peanutwrldcs@gmail.com>
2023-11-27 19:30:44 +0100

    Update connection.js

--- database/connection.js
+++ database/connection.js
@@ -1,23 +1,30 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USERNAME, process.env.DATABASE_PASSWORD, {
-  host: process.env.DATABASE_HOST,
-  dialect: 'mssql',
-  dialectOptions: {
-    encrypt: true,
-  },
-  logging: false,
-});
+const sequelize = new Sequelize(
+  process.env.DATABASE_NAME,
+  process.env.DATABASE_USERNAME,
+  process.env.DATABASE_PASSWORD,
+  {
+    host: process.env.DATABASE_HOST,
+    dialect: "mssql",
+    dialectOptions: {
+      options: {
+        encrypt: true,
+      },
+    },
+    logging: false,
+  }
+);
 
 const testConnection = async () => {
-    try {
-        await sequelize.authenticate();
-        console.log('Database connection has been established successfully.');
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
-    }
+  try {
+    await sequelize.authenticate();
+    console.log("Database connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
 };
-  
-testConnection();  
+
+testConnection();
 
 module.exports = { sequelize, testConnection };
